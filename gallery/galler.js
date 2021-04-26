d1313d013fe60b04164e1b110b3b925353ff5cec
imsahil2001 <sahilkkr56@gmail.com>
2021-04-27 00:18:08 +0530

    Initial commit

---
+++ gallery/galler.js
@@ -0,0 +1,6 @@
+var back=document.getElementsByClassName("picture");
+var hudd=document.getElementsByClassName("box");
+
+hudd.addEventListener("mouseenter",function name() {
+    back.classList.add('.black');
+});
